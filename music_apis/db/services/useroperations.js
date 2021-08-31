28d715c8abeceb23bdc6c747639f415a5a722535
AmanSinha26 <aman.sinha26@gmail.com>
2021-08-31 18:02:00 +0530

    first commit

---
+++ music_apis/db/services/useroperations.js
@@ -0,0 +1,22 @@
+const UserModel = require('../models/user');
+const userOperations = {
+    async register(userObject) {
+        const result = await UserModel.create(userObject);
+        console.log('Result is ', result);
+        return result._id;
+    },
+    async login({ userid, password }) {
+        const result = await UserModel.findOne({ "userid": userid, "password": password });
+        console.log('Login Result is ', result);
+        return result;
+    }
+    // login({userid, password}){
+    //     if(userid == password){
+    //         return "Welcome "+userid;
+    //     }
+    //     else{
+    //         return "Invalid userid or password";
+    //     }
+    // }
+}
+module.exports = userOperations;
